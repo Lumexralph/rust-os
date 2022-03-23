f2c866cb6e848184d8ea4355f30378675623b294
Olumide Ogundele <olumideralph@gmail.com>
2022-03-23 08:57:45 +0100

    make test output show in the console

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
 
 // function to handle panic, `!` means a function
@@ -15,10 +19,21 @@ mod vga_buffer;
 // where the panic happened and the optional panic message.
 // The function should never return, so it is marked as a diverging
 // function by returning the “never” type !.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+// Panic handler in test mode.
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop { }
 }
 
 #[no_mangle] // don't mangle the name of this function
@@ -28,9 +43,45 @@ pub extern "C" fn _start() -> ! {
     println!("Welcome to LumexOS {}\
          Current year - {}", "😎", 2022);
 
-    // unwrap panics if an error occurs. This isn’t a problem in our case,
-    // since writes to the VGA buffer never fails.
-    // write!(vga_buffer::WRITER.lock(), "Current year - {}", 2022).unwrap();
+    #[cfg(test)]
+        test_main();
 
     loop { }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion...");
+    assert_eq!(3,3);
+    serial_println!("[OK]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    // We use u32 because we specified the iosize of the isa-debug-exit device as 4 bytes.
+    // Both operations are unsafe, because writing to an I/O port can generally result in
+    // arbitrary behavior.
+    unsafe {
+        // 0xf4 is the port address/iobase of isa-debug-exit device.
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
